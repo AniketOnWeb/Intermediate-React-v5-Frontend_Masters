47aa7f091bc36f3a7c8cff776a81f1e2723ee018
Aniket Chaudhary <chaudharyaniket16@gmail.com>
2019-12-05 23:41:57 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParam";
 import Details from "./Details";
 
 export const App = () => {
   return (
     <div>
-      <h1 id="something-important"> Adopt Me</h1>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
       <Router>
         <SearchParams path="/" />
         <Details path="/details/:id" />
