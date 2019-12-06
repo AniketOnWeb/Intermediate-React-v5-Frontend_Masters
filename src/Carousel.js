d062e0a18fd6da8a38ed307cfb2caa3e85d14d92
Aniket Chaudhary <chaudharyaniket16@gmail.com>
2019-12-06 23:55:35 +0530

    added Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -6,12 +6,35 @@ class Carousel extends React.Component {
     active: 0
   };
 
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media(({ large }) => large);
+    }
+
+    return photos;
+  }
+
   render() {
-    const { photos, actiev } = this.state;
+    const { photos, active } = this.state;
 
     return (
       <div className="carousel">
         <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            //eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              src={photo}
+              data-index={index}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
       </div>
     );
   }
